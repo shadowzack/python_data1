1cbd586027d7e5dc5e1746aee65e5ca857e20f57
mahmod hasan <battleforge-war@hotmail.com>
2018-12-13 21:49:45 +0200

    adding html forrmat of our data, our main technology class

---
+++ tech.py
@@ -0,0 +1,11 @@
+class Tech:
+
+    def __init__(self, qustion, tags, qId,date,href):
+        self.qustion = qustion
+        self.tags = tags
+        self.qId = qId
+        self.date=date
+        self.href=href
+    def myfunc(self):
+        print("test")
+
--- test.py
+++ test.py
@@ -1,13 +1,21 @@
-#%%
+# %%
 
 from bs4 import BeautifulSoup
 import urllib.request
+from lxml import etree, html
+from html5print import HTMLBeautifier
+from html.parser import HTMLParser
+import tech 
 
-i=1
-Page=[]
-for i in range(1,6):
-    html = urllib.request.urlopen("https://stackoverflow.com/questions/tagged/javascript?sort=newest&page="+str(i)+"&pagesize=50")
-    soup = BeautifulSoup(html.read(),"html.parser")
-    Page.extend(soup.find_all("div","summary"))
-    print(i)
-print(len(Page))
+i = 1
+Page = []
+for i in range(1, 2):
+    htmlPage = urllib.request.urlopen(
+        "https://stackoverflow.com/questions/tagged/javascript?sort=newest&page="+str(i)+"&pagesize=50")
+    soup = BeautifulSoup(htmlPage.read(), "html.parser")
+    Page.extend(soup.find_all("div", "summary"))
+   # print(i)
+
+print(Page[0])
+temp=tech.Tech(1,1,1,1,1)
+print(str(temp))
