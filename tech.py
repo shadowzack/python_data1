0b7eda03b492f1af0ef5e57e36a520da03161308
mahmod hasan <battleforge-war@hotmail.com>
2018-12-14 18:25:37 +0200

    now all the data is inside and array of tech objects

--- tech.py
+++ tech.py
@@ -1,11 +1,12 @@
 class Tech:
 
-    def __init__(self, qustion, tags, qId,date,href):
+    def __init__(self, header, qustion, tags, qId, relativeTime, href):
         self.qustion = qustion
         self.tags = tags
         self.qId = qId
-        self.date=date
-        self.href=href
-    def myfunc(self):
-        print("test")
+        self.relativeTime = relativeTime
+        self.href = href
+        self.header = header
 
+    def __str__(self):
+        return "########### Tech Object: header= %s, qustion= %s, tags= %s, qId= %s,relativeTime= %s,href= %s \n\n" % (self.header, self.qustion, self.tags, self.qId, self.relativeTime, self.href)
--- test.py
+++ test.py
@@ -2,20 +2,48 @@
 
 from bs4 import BeautifulSoup
 import urllib.request
-from lxml import etree, html
 from html5print import HTMLBeautifier
-from html.parser import HTMLParser
-import tech 
+import tech
+import html
 
 i = 1
 Page = []
+techArray = []
 for i in range(1, 2):
     htmlPage = urllib.request.urlopen(
         "https://stackoverflow.com/questions/tagged/javascript?sort=newest&page="+str(i)+"&pagesize=50")
     soup = BeautifulSoup(htmlPage.read(), "html.parser")
     Page.extend(soup.find_all("div", "summary"))
-   # print(i)
+    tempQustionsArray = soup.find_all("div", "summary")
+    z = 0
+    for z in range(0, len(tempQustionsArray)):
+        header = (tempQustionsArray[z].find("a", "question-hyperlink")).text
+        print(header)
 
-print(Page[0])
-temp=tech.Tech(1,1,1,1,1)
-print(str(temp))
+        href = (tempQustionsArray[z].find("a", "question-hyperlink")).get("href")
+        print(href)
+
+        relativeTime = (tempQustionsArray[z].find("span", "relativetime")).get("title")
+        print(relativeTime)
+
+        qustion = (tempQustionsArray[z].find("div", "excerpt")).text
+        print(qustion)
+
+        qId = href.rsplit('/', 2)[1]
+        print(qId)
+
+        tags = []
+        tagesArraytempIndex = 0
+        tagesArraytemp = (tempQustionsArray[z].find_all("a", "post-tag"))
+        for tagesArraytempIndex in range(0, len(tagesArraytemp)):
+            tags.append(tagesArraytemp[tagesArraytempIndex].text)
+        print(tags)
+        
+        techArray.append(
+            tech.Tech(header, qustion, tags, qId, relativeTime, href))
+
+        print('-------------------------------------------------------------------')
+
+#printing using override
+for a in techArray:
+    print(a)
