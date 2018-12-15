2f2db836958116a99b7012471f6c4ede896ecc6a
Adham Khalilieh <adhamkhalilieh@Adhams-MacBook-Pro-2.local>
2018-12-15 21:17:00 +0200

    crawler edit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 from bs4 import BeautifulSoup
 import urllib.request
-from html5print import HTMLBeautifier
+# from html5print import HTMLBeautifier
 import tech
 import html
 from json import JSONEncoder
@@ -10,43 +10,62 @@ import tech
 import json
 import sys
 
-i = 1
+with open('languages.json') as f:
+    langes = json.load(f)
+    
+i = 1 
+maxPage = 10
+locker = 0 
 Page = []
 techArray = []
-for i in range(1, 2):
-    htmlPage = urllib.request.urlopen(
-        "https://stackoverflow.com/questions/tagged/javascript?sort=newest&page="+str(i)+"&pagesize=50")
-    soup = BeautifulSoup(htmlPage.read(), "html.parser")
-    Page.extend(soup.find_all("div", "summary"))
-    tempQustionsArray = soup.find_all("div", "summary")
-    z = 0
-    for z in range(0, len(tempQustionsArray)):
-        header = (tempQustionsArray[z].find("a", "question-hyperlink")).text
-        print(header)
+for lang in langes:
+    locker = 0
+    for i in range(1, maxPage):
+        try:
+            htmlPage = urllib.request.urlopen(
+                "https://stackoverflow.com/questions/tagged/"+lang+"?sort=newest&page="+str(i)+"&pagesize=50")
+        except:
+            break
+        soup = BeautifulSoup(htmlPage.read(), "html.parser")
 
-        href = (tempQustionsArray[z].find("a", "question-hyperlink")).get("href")
-        print(href)
+        ### getting Max page number
+        ### one time each string
+        if locker == 0 :
+            pagesNum = soup.find("div","pager fl").findAll("a")
+            maxPage = int(pagesNum[4].find("span").text)
+            locker=1
 
-        relativeTime = (tempQustionsArray[z].find("span", "relativetime")).get("title")
-        print(relativeTime)
+        ### questions crawler starts here 
+        Page.extend(soup.find_all("div", "summary"))
+        # tempQustionsArray = soup.find_all("div", "summary")
+        z = 0
+        for section in Page:
+            header = (section.find("a", "question-hyperlink")).text
+            print(header)
 
-        qustion = (tempQustionsArray[z].find("div", "excerpt")).text
-        print(qustion)
+            href = (section.find("a", "question-hyperlink")).get("href")
+            print(href)
 
-        qId = href.rsplit('/', 2)[1]
-        print(qId)
+            relativeTime = (section.find("span", "relativetime")).get("title")
+            print(relativeTime)
 
-        tags = []
-        tagesArraytempIndex = 0
-        tagesArraytemp = (tempQustionsArray[z].find_all("a", "post-tag"))
-        for tagesArraytempIndex in range(0, len(tagesArraytemp)):
-            tags.append(tagesArraytemp[tagesArraytempIndex].text)
-        print(tags)
-        
-        techArray.append(
-            tech.Tech(header, qustion, tags, qId, relativeTime, href))
+            qustion = (section.find("div", "excerpt")).text
+            print(qustion)
 
-        print('-------------------------------------------------------------------')
+            qId = href.rsplit('/', 2)[1]
+            print(qId)
+
+            tags = []
+            tagesArraytempIndex = 0
+            tagesArraytemp = (section.find_all("a", "post-tag"))
+            for tagesArraytempIndex in tagesArraytemp:
+                tags.append(tagesArraytempIndex.text)
+            print(tags)
+            
+            techArray.append(
+                tech.Tech(header, qustion, tags, qId, relativeTime, href))
+
+            print('-------------------------------------------------------------------')
 
 
 
@@ -58,7 +77,19 @@ for i in range(1, 2):
    # print(a)
    # json.dumps(a)
    #print(json.dumps(foo.__dict__))
-    sys.stdout = open("Data1.json", "w+")
+
+
+# with open('languages.json', 'w') as outfile:
+#     json.dump(langueges, outfile)
+
+
+    #opens file for stdout all prints are printed to the file
+
+
+
+    sys.stdout = open("Data1.json", "a")
     json_string = json.dumps([ob.__dict__ for ob in techArray])
     print(json_string)
     sys.stdout.close()
+
+
